b6b5d477b5643d76632cf967f096c9a7880c73d0
parthardeshanacs <parth.ardeshana@codeshastra.com>
2023-02-01 11:15:03 +0530

    Filter section work on learner page

--- src/store/Learner/reducer.js
+++ src/store/Learner/reducer.js
@@ -6,6 +6,7 @@ import {
   GET_LEARNER_COUNT_FAIL,
   DELETE_LEARNER_SUCCESS,
   DELETE_LEARNER_FAIL,
+  FILTER_STATUS_LEARNER,
 } from "./actionTypes"
 
 const INIT_STATE = {
@@ -56,6 +57,11 @@ const Learner = (state = INIT_STATE, action) => {
       return {
         ...state,
         error: action.payload,
+      }
+    case FILTER_STATUS_LEARNER:
+      return {
+        ...state,
+        data: action.payload,
       }
     default:
       return state
