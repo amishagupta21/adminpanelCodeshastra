d8d9c1c92fb8b4c655107a60a11041ca1a4e52d5
sharad9211 <sharad.kumar@codeshastra.com>
2023-06-13 21:45:14 +0530

    work on create-batches

--- src/pages/batches/EditNewModal.js
+++ src/pages/batches/EditNewModal.js
@@ -19,6 +19,8 @@ import {
 import { editNewBatch } from "store/actions"
 import { connect } from "react-redux"
 import BatchSchedule from "./BatchSchedule"
+import { post, getCourseData } from "../../helpers/api_helper"
+import * as url from "../../helpers/url_helper"
 
 const EditNewModal = ({
   onEditNewBatch,
@@ -29,10 +31,23 @@ const EditNewModal = ({
   setEditModal,
 }) => {
   const [editData, setEditData] = useState({ ...batchApi })
+  const [selectedCourseId, setSelectedCourseId] = useState("0")
+  const [courseIdData, setCourseIdData] = useState([])
 
   useEffect(() => {
     setEditData(batchApi)
   }, [batchApi])
+
+  useEffect(() => {
+    if (editModal) {
+      const getNewBatches = async () => {
+        const resp = await getCourseData(url.GET_MOODLE_COURSE)
+        setCourseIdData(resp.data)
+        return resp
+      }
+      getNewBatches()
+    }
+  }, [editModal])
 
   const updateBatches = event => {
     event.preventDefault()
@@ -178,15 +193,15 @@ const EditNewModal = ({
                   <Table responsive>
                     <thead>
                       <tr>
-                        <th>Mentor</th>
-                        <th>Learners Limit</th>
+                        {/* <th>Mentor</th>
+                        <th>Learners Limit</th> */}
                         <th>Start Date</th>
                         <th>End Date</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td>
+                        {/* <td>
                           <FormGroup>
                             <FormGroup className="select_box border-0">
                               <Input
@@ -224,7 +239,7 @@ const EditNewModal = ({
                               }
                             />
                           </FormGroup>
-                        </td>
+                        </td> */}
                         <td>
                           <Input
                             type="date"
@@ -261,6 +276,41 @@ const EditNewModal = ({
                   setEditData={setEditData}
                 />
               )}
+              <AccordionItem className="mb-3">
+                <AccordionHeader targetId="3">
+                  Moodle Course ID
+                  <i className="mdi mdi-information-outline font-size-16 ms-2"></i>
+                </AccordionHeader>
+                <AccordionBody accordionId="3">
+                  <Row>
+                    <Col md={4} style={{ paddingLeft: "33px" }}>
+                      <FormGroup>
+                        <Label>Course ID</Label>
+                        <Input
+                          name="select"
+                          onChange={e =>
+                            setEditData({
+                              ...editData,
+                              course: e.target.value,
+                            })
+                          }
+                          value={editData?.course}
+                          type="select"
+                        >
+                          <option value="0">Select Course ID</option>
+                          {courseIdData.map((item, index) => {
+                            return (
+                              <option key={index} value={editData?.courseid}>
+                                {item?.coursename}
+                              </option>
+                            )
+                          })}
+                        </Input>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                </AccordionBody>
+              </AccordionItem>
             </UncontrolledAccordion>
           </Col>
         </Row>
@@ -275,7 +325,7 @@ const EditNewModal = ({
           Cancel
         </Button>
         <Button color="primary" onClick={updateBatches} className="px-5">
-          Create
+          Update
         </Button>
       </ModalFooter>
     </Modal>
