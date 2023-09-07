0e98c2a163023e1d06eb99850a6e2af1ac4943a1
USER <amisha.gupta@codeshastra.com>
2023-09-07 22:57:27 +0530

    testing

--- src/components/Common/Breadcrumb.js
+++ src/components/Common/Breadcrumb.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom"
 import { Row, Col, BreadcrumbItem } from "reactstrap"
 
 const Breadcrumb = props => {
+  console.log(props)
   return (
     <Row>
       <Col xs="12">
