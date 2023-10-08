8779a662449d8ca565af1b11f5e268c26115e44c
USER <amisha.gupta@codeshastra.com>
2023-10-08 13:43:22 +0530

    description issue

--- src/pages/batches/BatchNewModal.js
+++ src/pages/batches/BatchNewModal.js
@@ -134,6 +134,7 @@ const BatchNewModal = ({ modal, toggle, setModal, setItem, item }) => {
       // batchName !== "" &&
       // description !== "" &&
       course !== "Select" &&
+      (moodleDetail[0]?.displayname || "") !== "" && 
       variantType !== "Select" &&
       // classLink !== "" &&
       mentor !== "Select" &&
@@ -147,6 +148,7 @@ const BatchNewModal = ({ modal, toggle, setModal, setItem, item }) => {
     description,
     course,
     variantType,
+    moodleDetail[0]?.displayname,
     classLink,
     mentor,
     learnersLimit,
@@ -725,7 +727,7 @@ const BatchNewModal = ({ modal, toggle, setModal, setItem, item }) => {
                           ])
                         }
                       >
-                        Add A Schedule +!11
+                        Add A Schedule
                       </button>
                     </Col>
                   </Row>
